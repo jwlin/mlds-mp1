869278729c2e1aa5b50a578692dc820599f7276c
Yumichi <kinki5406@gmail.com>
2015-10-31 14:10:28 +0800

    10/31

--- rnn.example.py
+++ rnn.example.py
@@ -4,63 +4,29 @@ import numpy as np
 import sys
 from itertools import izip
 import time
+from rnnparser import TestParser
 
-# Min/max sequence length
-MIN_LENGTH = 50
-MAX_LENGTH = 55
+
 # Number of units in the hidden (recurrent) layer
-N_HIDDEN = 100
+N_HIDDEN = 32
 # input
-N_INPUT = 2
+N_INPUT = 48
 # output
-N_OUTPUT = 1
+N_OUTPUT = 48
 
-# from https://github.com/Lasagne/Lasagne/blob/master/examples/recurrent.py
-def gen_data(min_length=MIN_LENGTH, max_length=MAX_LENGTH):
-    '''
-    Generate a sequences for the "add" task, e.g. the target for the
-    following
-    ``| 0.5 | 0.7 | 0.3 | 0.1 | 0.2 | ... | 0.5 | 0.9 | ... | 0.8 | 0.2 |
-      |  0  |  0  |  1  |  0  |  0  |     |  0  |  1  |     |  0  |  0  |``
-    would be 0.3 + .9 = 1.2.  This task was proposed in [1]_ and explored in
-    e.g. [2]_.
-    Parameters
-    ----------
-    min_length : int
-        Minimum sequence length.
-    max_length : int
-        Maximum sequence length.
-    Returns
-    -------
-    X : np.ndarray
-        Input to the network, of shape (length, 2), where the last
-        dimension corresponds to the two sequences shown above.
-    y : np.ndarray
-        Correct output for each sample (a scalar).
-    '''
-    # Generate x_seq
-    length = np.random.randint(min_length, max_length)	
-    x_seq = np.concatenate([np.random.uniform(size=(length, 1)),
-                        np.zeros((length, 1))],
-                       axis=-1)
-    # Set the second dimension to 1 at the indices to add
-    x_seq[np.random.randint(length/10), 1] = 1
-    x_seq[np.random.randint(length/2, length), 1] = 1
-    # Multiply and sum the dimensions of x_seq to get the target value
-    y_hat = np.sum(x_seq[:, 0]*x_seq[:, 1])
-    return x_seq, y_hat
+
 
 # what can we get from gen_data()
 #print gen_data()
 
 x_seq = T.matrix('input')
-y_hat = T.scalar('target')
+y_hat = T.matrix('target')
 
-Wi = theano.shared( np.random.randn(N_INPUT,N_HIDDEN) )
-bh = theano.shared( np.zeros(N_HIDDEN) )
-Wo = theano.shared( np.random.randn(N_HIDDEN,N_OUTPUT) )
-bo = theano.shared( np.zeros(N_OUTPUT) )
-Wh = theano.shared( np.random.randn(N_HIDDEN,N_HIDDEN) )
+Wi = theano.shared( np.random.randn(N_INPUT,N_HIDDEN).astype(np.float32))
+bh = theano.shared( np.zeros(N_HIDDEN).astype(np.float32) )
+Wo = theano.shared( np.random.randn(N_HIDDEN,N_OUTPUT).astype(np.float32) )
+bo = theano.shared( np.zeros(N_OUTPUT) .astype(np.float32) )
+Wh = theano.shared( np.random.randn(N_HIDDEN,N_HIDDEN).astype(np.float32) )
 parameters = [Wi,bh,Wo,bo,Wh]
 
 def sigmoid(z):
@@ -72,42 +38,65 @@ def step(x_t,a_tm1,y_tm1):
         y_t = T.dot(a_t,Wo) + bo
         return a_t, y_t
 
-a_0 = theano.shared(np.zeros(N_HIDDEN))
-y_0 = theano.shared(np.zeros(N_OUTPUT))
+a_0 = theano.shared(np.zeros(N_HIDDEN).astype(np.float32))
+y_0 = theano.shared(np.zeros(N_OUTPUT).astype(np.float32))
 
 [a_seq,y_seq],_ = theano.scan(
                         step,
                         sequences = x_seq,
                         outputs_info = [ a_0, y_0 ],
-			truncate_gradient=-1
+                        truncate_gradient=-1
                 )
 
-y_seq_last = y_seq[-1][0] # we only care about the last output 
-cost = T.sum( ( y_seq_last - y_hat )**2 ) 
+ # we only care about the last output 
+cost = T.sum( ( y_seq - y_hat )**2 ) 
 
 gradients = T.grad(cost,parameters)
 
 def MyUpdate(parameters,gradients):
-	mu =  np.float32(0.001)
+	mu =  np.float32(0.0001)
 	parameters_updates = [(p,p - mu * g) for p,g in izip(parameters,gradients) ] 
 	return parameters_updates
 
 rnn_test = theano.function(
         inputs= [x_seq],
-        outputs=y_seq_last
+        outputs=y_seq,
+        allow_input_downcast=True
 )
 
 rnn_train = theano.function(
         inputs=[x_seq,y_hat],
         outputs=cost,
-	updates=MyUpdate(parameters,gradients)
+        updates=MyUpdate(parameters,gradients),
+        allow_input_downcast=True
+
 )
 
-for i in range(10000000):
-	x_seq, y_hat = gen_data()
-	print "iteration:", i, "cost:",  rnn_train(x_seq,y_hat)
+x_seq, y_hat = TestParser.load()
 
-for i in range(10):
-	x_seq, y_hat = gen_data()
-	print "reference", y_hat, "RNN output:", rnn_test(x_seq)
 
+
+
+for i in range(1):
+    
+    for j in xrange(len(x_seq)):
+        c_cost = rnn_train(x_seq[j],y_hat[j])/len(y_hat[j])
+        print"iteration:", j, "cost:",  c_cost
+        
+
+correct = 0
+all = 0
+max_index = 0
+t_start = time.time()
+label_index = TestParser.label.values()
+for i in xrange(len(x_seq)):
+    y_a = rnn_test(x_seq[i])
+    y_a = list(y_a)
+    for j in  xrange(len(y_a)):
+        y_a[j] = list(y_a[j])
+        max_index = y_a[j].index(max(y_a[j]))   
+        if TestParser.chr_keys[max_index] == label_index[all]:
+            correct+=1
+        all+=1
+t_end = time.time()
+print "correct = ",correct ,"/",all ,t_end-t_start," sec." 
---
+++ rnnparser.py
@@ -0,0 +1,67 @@
+import numpy as np
+import time
+
+class TestParser:
+    x_seq=[]
+    y_hat=[]
+    label = {}
+    label_map = {}
+    chr_map = {}
+    chr_keys = []
+    dimension_x = 0
+    dimension_y = 0
+ 
+    @classmethod
+    def load(cls):
+        t_start = time.time()
+        label_keys = np.genfromtxt('phones/48_39.map', usecols=0, dtype=str)
+        label_raw_data = np.genfromtxt('phones/48_39.map', usecols=[1],dtype=str)
+        cls.label_map = {key: row for key, row in zip(label_keys, label_raw_data)}
+
+        cls.chr_keys = np.genfromtxt('48_idx_chr.map_b', usecols=0, dtype=str)
+        chr_raw_data = np.genfromtxt('48_idx_chr.map_b', usecols=[1,2],dtype=None)
+        cls.chr_map = {key: row for key, row in zip(cls.chr_keys, chr_raw_data)}
+        
+        with open('label/train.lab', 'r') as f:
+            for line in f:
+                line = line.replace('\n', '')
+                token = line.split(',')
+                sample_id = token[0]
+                cls.label[sample_id] = token[1]
+
+        with open('posteriorgram/train.post', 'r') as f:
+            frame = ''
+            id_index = -1
+            for line in f:
+                #print(line)
+                line = line.replace('\n', '')
+                token = line.split(' ')
+                index = token[0].rfind('_')
+                y = TestParser.load_y(cls.label[token[0]])
+
+                if not frame == token[0][0:index]:
+                    frame = token[0][0:index]
+                    id_index = id_index + 1
+                    cls.x_seq.append([])
+                    cls.y_hat.append([])
+
+                attr = []
+                for attr_str in token[1:]: attr.append(float(attr_str))
+                cls.x_seq[id_index].append(attr)
+                cls.y_hat[id_index].append(y)
+        cls.dimension_x = len(TestParser.x_seq[0][0])
+        cls.dimension_y = len(TestParser.y_hat[0][0])
+
+        t_end = time.time()
+        print 'data loaded. loading time: %f sec' % (t_end - t_start)
+        return cls.x_seq , cls.y_hat
+    @classmethod
+    def load_y(cls, label):
+        y = [0]*48
+        y[ cls.chr_map[cls.label_map[label]][0] ] = 1
+        return y
+'''
+TestParser.load()
+print(len(TestParser.x_seq), len(TestParser.x_seq[0]), (TestParser.x_seq[0][2]))
+print(len(TestParser.y_hat), len(TestParser.y_hat[0]), (TestParser.y_hat[0][2]))
+'''
