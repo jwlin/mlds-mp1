0f0cea7762fc484d003da21d820a431236250fb1
rabbit159t <b00902045@gmail.com>
2015-11-03 00:58:27 +0800

    add load & save function, easy to change parameters value

--- rnn.example.py
+++ rnn.example.py
@@ -6,27 +6,31 @@ from itertools import izip
 import time
 from rnnparser import TestParser
 
+post_file = 'posteriorgram/train2.post'
+test_file = 'posteriorgram/test.post'
+label_file = 'label/train.lab'
+label_map_file = 'phones/48_39.map'
+chr_map_file = '48_idx_chr.map_b'
+epoch_cycle = 1
+mu =  np.float32(0.001)
+TestParser.load(label_map_file, chr_map_file, label_file, post_file)
 
 # Number of units in the hidden (recurrent) layer
 N_HIDDEN = 32
-# input
-N_INPUT = 48
-# output
-N_OUTPUT = 48
-
-
-
-# what can we get from gen_data()
-#print gen_data()
+N_INPUT = TestParser.dimension_x
+N_OUTPUT = TestParser.dimension_y
 
 x_seq = T.matrix('input')
 y_hat = T.matrix('target')
 
-Wi = theano.shared( np.random.randn(N_INPUT,N_HIDDEN).astype(np.float32))
-bh = theano.shared( np.zeros(N_HIDDEN).astype(np.float32) )
-Wo = theano.shared( np.random.randn(N_HIDDEN,N_OUTPUT).astype(np.float32) )
-bo = theano.shared( np.zeros(N_OUTPUT) .astype(np.float32) )
-Wh = theano.shared( np.random.randn(N_HIDDEN,N_HIDDEN).astype(np.float32) )
+Wi,bh,Wo,bo,Wh = TestParser.load_matrix(fname = "rnn_parameter.txt")
+'''
+Wi = TestParser.load_matrix(N_INPUT, N_HIDDEN, name='Wi')
+bh = TestParser.load_matrix(N_HIDDEN, name='bh')
+Wo = TestParser.load_matrix(N_HIDDEN,N_OUTPUT, name='Wo')
+bo = TestParser.load_matrix(N_OUTPUT, name='bo')
+Wh = TestParser.load_matrix(N_HIDDEN,N_HIDDEN, name='Wh')
+'''
 parameters = [Wi,bh,Wo,bo,Wh]
 
 def sigmoid(z):
@@ -41,25 +45,24 @@ a_0 = theano.shared(np.zeros(N_HIDDEN).astype(np.float32))
 y_0 = theano.shared(np.zeros(N_OUTPUT).astype(np.float32))
 
 [a_seq,y_seq],_ = theano.scan(
-                        step,
-                        sequences = x_seq,
-                        outputs_info = [ a_0, y_0 ],
-                        truncate_gradient=-1
+                    step,
+                    sequences = x_seq,
+                    outputs_info = [ a_0, y_0 ],
+                    truncate_gradient=-1
                 )
 
- # we only care about the last output 
-cost = T.sum( ( y_seq - y_hat )**2 ) 
+y_seq_last = y_seq # we only care about the last output 
+cost = T.sum( ( y_seq_last - y_hat )**2 ) 
 
 gradients = T.grad(cost,parameters)
 
 def MyUpdate(parameters,gradients):
-    mu =  np.float32(0.0001)
     parameters_updates = [(p,p - mu * g) for p,g in izip(parameters,gradients) ] 
     return parameters_updates
 
 rnn_test = theano.function(
         inputs= [x_seq],
-        outputs=y_seq,
+        outputs=y_seq_last,
         allow_input_downcast=True
 )
 
@@ -70,24 +73,26 @@ rnn_train = theano.function(
         allow_input_downcast=True
 )
 
-x_seq, y_hat = TestParser.load()
-
-for i in xrange(1):
-    for j in xrange(len(x_seq)):
-        c_cost = rnn_train(x_seq[j],y_hat[j])/len(y_hat[j])
+for i in xrange(epoch_cycle):
+    for j in xrange(len(TestParser.x_seq)):
+        c_cost = rnn_train(TestParser.x_seq[j], TestParser.y_hat[j])/len(TestParser.y_hat[j])
         print"iteration:", j, "cost:",  c_cost
-        
+    #print(i,'th epoch')
+TestParser.save_parameters(parameters)
+
+result, test_id, ans = TestParser.load_test_data(post_file)
 t_start = time.time()
 correct = 0
 all = 0
 max_index = 0
-for i in xrange(len(x_seq)):
-    y_a = rnn_test(x_seq[i])
+for i in xrange(len(result)):
+    y_a = rnn_test(result[test_id[i]])
+    #y_a = TestParser.x_seq[test_id[i]]
     y_a = list(y_a)
     for j in xrange(len(y_a)):
         y_a[j] = list(y_a[j])
         max_index = y_a[j].index(max(y_a[j]))
-        if TestParser.chr_keys[max_index] == TestParser.label_ans[all]: correct+=1
+        if TestParser.label[ans[test_id[i]][j]] == TestParser.label_keys[max_index]: correct+=1
         all+=1
 t_end = time.time()
 print "correct = ",correct ,"/",all ,t_end-t_start," sec." 
--- rnnparser.py
+++ rnnparser.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+import theano
+import random
 
 class TestParser:
     x_seq=[]
@@ -9,23 +11,25 @@ class TestParser:
 
     label_map = {}
     label_keys = []
+    label_num={}
     chr_map = {}
     chr_keys = []
     dimension_x = 0
     dimension_y = 0
  
     @classmethod
-    def load(cls):
+    def load(cls, label_map_file, chr_map_file, label_file, post_file):
         t_start = time.time()
-        cls.label_keys = np.genfromtxt('phones/48_39.map', usecols=0, dtype=str)
-        label_raw_data = np.genfromtxt('phones/48_39.map', usecols=[1],dtype=str)
+        cls.label_keys = np.genfromtxt(label_map_file, usecols=0, dtype=str)
+        for i in xrange(len(cls.label_keys)): cls.label_num[cls.label_keys[i]] = i
+        label_raw_data = np.genfromtxt(label_map_file, usecols=[1],dtype=str)
         cls.label_map = {key: row for key, row in zip(cls.label_keys, label_raw_data)}
 
-        cls.chr_keys = np.genfromtxt('48_idx_chr.map_b', usecols=0, dtype=str)
-        chr_raw_data = np.genfromtxt('48_idx_chr.map_b', usecols=[1,2],dtype=None)
+        cls.chr_keys = np.genfromtxt(chr_map_file, usecols=0, dtype=str)
+        chr_raw_data = np.genfromtxt(chr_map_file, usecols=[1,2],dtype=None)
         cls.chr_map = {key: row for key, row in zip(cls.chr_keys, chr_raw_data)}
         
-        with open('label/train.lab', 'r') as f:
+        with open(label_file, 'r') as f:
             for line in f:
                 line = line.replace('\n', '')
                 token = line.split(',')
@@ -33,7 +37,7 @@ class TestParser:
                 cls.label[sample_id] = token[1]
                 cls.label_ans.append(token[1])
 
-        with open('posteriorgram/train.post', 'r') as f:
+        with open(post_file, 'r') as f:
             frame = ''
             id_index = -1
             for line in f:
@@ -58,12 +62,150 @@ class TestParser:
 
         t_end = time.time()
         print 'data loaded. loading time: %f sec' % (t_end - t_start)
-        return cls.x_seq , cls.y_hat
+        #return cls.x_seq , cls.y_hat
+
     @classmethod
     def load_y(cls, label):
         y = [0]*48
-        y[ cls.chr_map[cls.label_map[label]][0] ] = 1
+        #y[ cls.chr_map[cls.label_map[label]][0] ] = 1
+        y[ cls.label_num[label] ] = 1
         return y
-'''
-TestParser.load()
-'''
+
+    @classmethod
+    def load_test_data(cls, fname):
+    	print 'loading test data...'
+        t_start = time.time()
+        test_matrix={}
+        test_id=[]
+        ans={}
+        tmp_ans=[]
+        tmp=[]
+        with open(fname, 'r') as f:
+            frame = ''
+            id_index = -1
+            for line in f:
+                #print(line)
+                line = line.replace('\n', '')
+                token = line.split(' ')
+                index = token[0].rfind('_')
+
+                if not frame == token[0][0:index]:
+                    frame = token[0][0:index]
+                    test_id.append(frame)
+                    id_index = id_index + 1
+                    tmp.append([])
+                    tmp_ans.append([])
+
+                attr = []
+                for attr_str in token[1:]: attr.append(float(attr_str))
+                tmp[id_index].append(attr)
+                test_matrix[frame] = tmp[id_index]
+
+                tmp_ans[id_index].append(token[0])
+                ans[frame] = tmp_ans[id_index]
+        
+        '''
+        cls.label_keys = np.genfromtxt('../dataset/phones/48_39.map', usecols=0, dtype=str)
+        with open('../dataset/label/train.lab', 'r') as f:
+            for line in f:
+                line = line.replace('\n', '')
+                token = line.split(',')
+                sample_id = token[0]
+                cls.label[sample_id] = token[1]
+                cls.label_ans.append(token[1])
+        '''
+
+        t_end = time.time()
+        print 'test data loaded. loading time: %f sec' % (t_end - t_start)
+        return test_matrix, test_id, ans
+
+    @classmethod
+    def load_matrix(cls, m=None, n=None, name=None, fname=None):
+        print('init parameters')
+        if fname:
+            with open(fname, 'r') as f:
+                if name:
+                    w_all = []
+                    line = f.readline() 
+                    token = line.split(' ')
+                    for i in xrange(len(token)):
+                        if token[i] == name:
+                            data = f.read()
+                            data_token = data.split('\n')
+                            matrix_data = data_token[2*i-1].split(',')
+                            num_data = data_token[2*i-2].split(',')
+                            data_x = int(num_data[0])
+                            data_y = int('0'+num_data[1][1:])
+                            if data_y != 0:
+                                #print(data_y)
+                                for x in range(data_x):
+                                    w = []
+                                    for y in range(data_y):
+                                       w.append(float(matrix_data[x*data_y+y]))
+                                    w_all.append(w)
+                            else:
+                                w = []
+                                for x in range(data_x):
+                                    w.append(float(matrix_data[x]))
+                                w_all = w
+                            return theano.shared(np.array(w_all).astype(np.float32),name)
+                else:
+                    w_all_matrix = []
+                    line = f.readline()
+                    token = line.split(' ')
+                    data = f.read()
+                    data_token = data.split('\n')           
+                    i=0                 
+                    while i < len(token)-2:
+                        w_all = []
+                        #print(i)
+                        matrix_data = data_token[2*i+1].split(',')
+                        num_data = data_token[2*i].split(',')
+                        #print(data_token[2*i])
+                        data_x = int(num_data[0])
+                        data_y = int('0'+num_data[1][1:])
+                        if data_y != 0:
+                            #print(data_y)
+                            for x in range(data_x):
+                                w = []
+                                for y in range(data_y):
+                                   w.append(float(matrix_data[x*data_y+y]))
+                                w_all.append(w)
+                        else:
+                            w = []
+                            for x in range(data_x):
+                                w.append(float(matrix_data[x]))
+                            w_all = w
+                        w_all_matrix.append(theano.shared(np.array(w_all).astype(np.float32),token[i+1]))
+                        i+=1
+                return w_all_matrix                     
+                # with open(fname, w):
+                # load file into theano shared variablie
+                # return theano.shared( np().astype(np.float32) )
+            pass
+        else:
+            if n:
+                return theano.shared( np.random.uniform(-1,1,(m, n)).astype(np.float32), name)
+            else:
+                #return theano.shared( np.random.uniform(-1,1,(m)).astype(np.float32), name)
+                return theano.shared( np.zeros(m).astype(np.float32), name)
+
+    @classmethod
+    def save_parameters(cls, parameters):
+        # save parameters in a way that the data can be easily loaded by theano
+        #np.set_printoptions(threshold=2**32)
+        print('save all parameters')
+        with open('rnn_parameter.txt', 'w') as f:
+            f.write('parameters: ')
+            for p in parameters:
+                f.write( p.name +' ')
+            f.write('\n')
+            for p in parameters:
+                t = str(p.get_value().shape).replace("(",'')
+                t = str(t).replace(")",'')
+                f.write(t + '\n')
+                t = p.get_value().tolist()
+                t = str(t).replace("[",'')
+                t = str(t).replace("]",'')
+                f.write("%s\n" %t )
+        #pass
